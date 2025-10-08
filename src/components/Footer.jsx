a95b8b6693dec6c88fa29207ffdf3494165fe371
allanpdev <allanpires224@gmail.com>
2025-10-08 08:36:15 -0300

    z

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 const FooterSection = styled.footer`
     &{ 
         position: relative;
+        background: #f4f5ff;
     }
     #footer-container{
         display: flex;
